dd1f953aff9afe14464eaf1a194ad6ce7a40e368
Artur Ciocanu <ciocanu@adobe.com>
2024-07-24 00:59:54 +0300

    Adding multiple Spring Boot enhancements
    Signed-off-by: Artur Ciocanu <ciocanu@adobe.com>

---
+++ sdk-springboot/sdk-springboot-autoconfigure/src/main/java/io/dapr/spring/boot/autoconfigure/statestore/DaprStateStoreAutoConfiguration.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2024 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.dapr.spring.boot.autoconfigure.statestore;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import io.dapr.spring.boot.autoconfigure.client.DaprClientAutoConfiguration;
+import io.dapr.spring.data.DaprKeyValueAdapterResolver;
+import io.dapr.spring.data.DaprKeyValueTemplate;
+import io.dapr.spring.data.KeyValueAdapterResolver;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.keyvalue.core.KeyValueAdapter;
+
+@AutoConfiguration(after = DaprClientAutoConfiguration.class)
+@ConditionalOnClass({DaprClient.class, KeyValueAdapter.class})
+@EnableConfigurationProperties(DaprStateStoreProperties.class)
+public class DaprStateStoreAutoConfiguration {
+
+  /**
+   * Creates a {@link KeyValueAdapterResolver} bean if it is not already defined.
+   *
+   * @param daprClientBuilder        The Dapr client builder.
+   * @param mapper                   The object mapper.
+   * @param daprStateStoreProperties The Dapr state store properties.
+   * @return A {@link KeyValueAdapterResolver} bean.
+   */
+  @Bean
+  @ConditionalOnMissingBean
+  public KeyValueAdapterResolver keyValueAdapterResolver(DaprClientBuilder daprClientBuilder, ObjectMapper mapper,
+                                                         DaprStateStoreProperties daprStateStoreProperties) {
+    DaprClient daprClient = daprClientBuilder.build();
+    String stateStoreName = daprStateStoreProperties.getName();
+    String bindingName = daprStateStoreProperties.getBinding();
+
+    return new DaprKeyValueAdapterResolver(daprClient, mapper, stateStoreName, bindingName);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public DaprKeyValueTemplate keyValueTemplate(KeyValueAdapterResolver keyValueAdapterResolver) {
+    return new DaprKeyValueTemplate(keyValueAdapterResolver);
+  }
+
+}
---
+++ sdk-springboot/sdk-springboot-autoconfigure/src/test/java/io/dapr/spring/boot/autoconfigure/statestore/DaprStateStoreAutoConfigurationTests.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2024 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.dapr.spring.boot.autoconfigure.statestore;
+
+import io.dapr.spring.boot.autoconfigure.client.DaprClientAutoConfiguration;
+import io.dapr.spring.data.DaprKeyValueTemplate;
+import io.dapr.spring.data.KeyValueAdapterResolver;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit tests for {@link DaprStateStoreAutoConfiguration}.
+ */
+class DaprStateStoreAutoConfigurationTests {
+  private static final DaprStateStoreProperties PROPERTIES = createProperties();
+  private static final KeyValueAdapterResolver RESOLVER = createResolver();
+  private static final AutoConfigurations AUTO_CONFIGURATIONS = AutoConfigurations.of(
+      JacksonAutoConfiguration.class,
+      DaprClientAutoConfiguration.class,
+      DaprStateStoreAutoConfiguration.class
+  );
+
+  private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+      .withBean(DaprStateStoreProperties.class, () -> PROPERTIES)
+      .withBean(KeyValueAdapterResolver.class, () -> RESOLVER)
+      .withConfiguration(AUTO_CONFIGURATIONS);
+
+  private static DaprStateStoreProperties createProperties() {
+    DaprStateStoreProperties properties = new DaprStateStoreProperties();
+    properties.setName("kvstore");
+    properties.setBinding("kvbinding");
+    return properties;
+  }
+
+  private static KeyValueAdapterResolver createResolver() {
+    return () -> null;
+  }
+
+  @Test
+  void daprKeyValueTemplate() {
+    contextRunner.run(context -> {
+      assertThat(context).hasSingleBean(DaprKeyValueTemplate.class);
+    });
+  }
+
+}
