f94cc4d6f1f06f6881ef736ce13c7cd93d3c3494
Artur Ciocanu <ciocanu@adobe.com>
2024-08-06 21:01:54 +0300

    Ensure waitForSidecar() is used in all tests
    Signed-off-by: Artur Ciocanu <ciocanu@adobe.com>

--- sdk-tests/src/test/java/io/dapr/it/spring/data/AbstractPostgreSQLBaseIT.java
+++ sdk-tests/src/test/java/io/dapr/it/spring/data/AbstractPostgreSQLBaseIT.java
@@ -57,8 +57,6 @@ public abstract class AbstractPostgreSQLBaseIT extends AbstractDaprSpringDataBas
   static void beforeAll() {
     org.testcontainers.Testcontainers.exposeHostPorts(8080);
 
-    DAPR_CONTAINER.start();
-
     System.setProperty("dapr.grpc.port", Integer.toString(DAPR_CONTAINER.getGrpcPort()));
     System.setProperty("dapr.http.port", Integer.toString(DAPR_CONTAINER.getHttpPort()));
   }
